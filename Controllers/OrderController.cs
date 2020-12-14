67663d3ae5efa48a5ccfaeeaa5e70be50a1197b8
Alexander Zakharkin1 <alexander_zakharkin1@epam.com>
2020-12-14 17:44:12 +0300

    init commit

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using FormCalc.Models;
+using System.Threading.Tasks;
+
+namespace FormCalc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        OrderContext db;
+        public OrderController(OrderContext context)
+        {
+            db = context;
+            if (!db.Orders.Any())
+            {
+                db.Orders.Add(new Order { ClientId = 0, ClientName = "Best Company", Cost = 1000.0});
+            }
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormCalc.Models
+{
+    public class Order
+    {
+        public int ClientId { get; set; }
+        public string ClientName { get; set; }
+        public double Cost { get; set; }
+    }
+}
