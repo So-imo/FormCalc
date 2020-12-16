4b325993129a52e70ca535455eb5adb2b6963d3b
Alexander Zakharkin1 <alexander_zakharkin1@epam.com>
2020-12-16 23:42:11 +0300

    OrderController implemented

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -18,7 +18,67 @@ namespace FormCalc.Controllers
             if (!db.Orders.Any())
             {
                 db.Orders.Add(new Order { ClientId = 0, ClientName = "Best Company", Cost = 1000.0});
+                db.Orders.Add(new Order { ClientId = 1, ClientName = "Worst Company", Cost = 100.0 });
+                db.SaveChanges();
             }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> Get()
+        {
+            return await db.Orders.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> Get(int id)
+        {
+            Order order = await db.Orders.FirstOrDefaultAsync(x => x.ClientId == id);
+            if (order == null)
+                return NotFound();
+            return new ObjectResult(order);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Order>> Post(Order order)
+        {
+            if (order == null)
+            {
+                return BadRequest();
+            }
+
+            db.Orders.Add(order);
+            await db.SaveChangesAsync();
+            return Ok(order);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Order>> Put(Order order)
+        {
+            if (order == null)
+            {
+                return BadRequest();
+            }
+            if (!db.Orders.Any(x => x.ClientId == order.ClientId))
+            {
+                return NotFound();
+            }
+
+            db.Update(order);
+            await db.SaveChangesAsync();
+            return Ok(order);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Order>> Delete(int clientId)
+        {
+            Order order= db.Orders.FirstOrDefault(x => x.ClientId == clientId);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            db.Orders.Remove(order);
+            await db.SaveChangesAsync();
+            return Ok(order);
         }
     }
 }
